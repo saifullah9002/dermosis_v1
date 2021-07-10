6700a98f30a45dffc99bb8630a9c33c9f3305baa
unknown <saifullah9002@gmail.com>
2021-07-11 03:19:27 +0500

    appointment service fixed. sort of

--- src/services/scheduler.service.js
+++ src/services/scheduler.service.js
@@ -29,11 +29,12 @@ const scheduleAppointmentNotifications = async (appointments) => {
     appointments.forEach(async appointment => {
         const date = new Date(appointment.appointmentDateTime);
         const call = await callService.createCall({ appointmentId: appointment._id });
-        scheduler.scheduleJob(date, function () {
+        patientData = await userService.getUserById(appointment.patientId);
+        doctorData = await userService.getUserById(appointment.doctorId);
+        scheduler.scheduleJob(date, function() {
             const doctorScoketId = socketController.connectedClients.get(String(appointment.doctorId));
             const patientScoketId = socketController.connectedClients.get(String(appointment.patientId));
-            patientData = await userService.getUserById(appointment.patientId);
-            doctorData = await userService.getUserById(appointment.doctorId);
+            
             io.to(doctorScoketId).emit("appointment starting", [call._id,patientData.firstname + " " + patientData.lastname ]);
             io.to(patientScoketId).emit("appointment starting", [call._id,doctorData.firstname + " " + doctorData.lastname]);
         });
